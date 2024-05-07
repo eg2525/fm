8dc6e0d4a912fd6ed38625c17294612cbfb2d3a8
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-05-07 16:26:36 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from tax import tax_mapping, tax_adjustment
 from PL_process import mapping_df, PL_mapping, adjustment_df, dropping_df, output_to_csv
 
 folder_path = st.file_uploader("P/Lファイルを複数アップロードしてください", type=['xlsx'], accept_multiple_files=True)
-uploaded_Withfiles = st.file_upoader("引出金ファイルを複数アップロードしてください", type=['xlsx'], accept_multiple_files=True)
+uploaded_Withfiles = st.file_uploader("引出金ファイルを複数アップロードしてください", type=['xlsx'], accept_multiple_files=True)
 
 # ファイルがアップロードされた後、'OK'ボタンが押されるのを待つ
 if folder_path is not None and uploaded_Withfiles is not None:
