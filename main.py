cd9ee3dfa1588e3b18230e996e55d639404a6511
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-05-07 17:26:59 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -21,17 +21,17 @@ if uploaded_files is not None and uploaded_Withfiles is not None:
 		output_dataframes = initialize_output_dataframes(dataframes)
 
 		# 利用可能なすべてのデータフレームのキーを表示
-		if output_dataframes:
+		if dataframes:
 		    st.write("利用可能なデータフレームのキー:")
-		    for key in output_dataframes.keys():
+		    for key in dataframes.keys():
 		        st.write(key)
 		else:
 		    st.write("データフレームはまだ生成されていません。")
 
 		# ドロップダウンメニューからデータフレームを選択
-		file_name_to_view = st.selectbox("データフレームを選択してください", list(output_dataframes.keys()))
+		file_name_to_view = st.selectbox("データフレームを選択してください", list(dataframes.keys()))
 
 		# 選択されたデータフレームを表示
 		if file_name_to_view:
-		    st.dataframe(output_dataframes[file_name_to_view])
+		    st.dataframe(dataframes[file_name_to_view])
 
