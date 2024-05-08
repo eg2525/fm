b15e9af759e82794e65f6eefd9c1aaa93b2c9975
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-05-08 10:07:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from PL_process import mapping_df, PL_mapping, adjustment_df, dropping_df
 st.title('FM自動仕訳作成')
 
 st.markdown('''
-    ### 手順 
-    ####※月一回のみ。全店の仕訳が作成されます。
+    ### 手順 ※月一回のみ。全店の仕訳が作成されます。
     1. P/Lデータを分割→Acrobatでexcel化→アップロード
     2. 引出金データを分割→Acrobatでexcel化→アップロード
     3. 消費税データcsvをアップロード
